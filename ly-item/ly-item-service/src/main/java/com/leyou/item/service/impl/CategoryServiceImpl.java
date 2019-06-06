1dce44be9a0b69395cd87b78cbeab6038854a5e9
386592336@qq.com <386592336@qq.com>
2019-06-06 16:20:36 +0800

    商品分类查询

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.leyou.item.service.impl;
+
+import com.leyou.common.enums.ExceptionEnum;
+import com.leyou.common.exception.LyException;
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryDao;
+
+    @Override
+    public List<Category> queryCategoryListByPid(Long pid) {
+        // 查询条件，mapper会把对象中的非空属性作为查询条件
+        Category t = new Category();
+        t.setParentId(pid);
+        List<Category> list = categoryDao.select(t);
+        // 判断结果
+        if (CollectionUtils.isEmpty(list)) {
+            throw new LyException(ExceptionEnum.CATEGORY_NOT_FOND);
+        }
+        return list;
+    }
+}
