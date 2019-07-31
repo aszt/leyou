8875dd470adb614162793dc3cee4f98c61c5a34b
386592336@qq.com <386592336@qq.com>
2019-07-31 14:03:33 +0800

    FastDFS添加

--- ly-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
+++ ly-upload/src/main/java/com/leyou/upload/service/impl/UploadServiceImpl.java
@@ -1,31 +1,45 @@
 package com.leyou.upload.service.impl;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.upload.config.UploadProperties;
 import com.leyou.upload.service.UploadService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 @Slf4j
+@EnableConfigurationProperties(UploadProperties.class)
 public class UploadServiceImpl implements UploadService {
 
-    private static final List<String> ALLOW_TYPES = Arrays.asList("image/png", "image/jpeg", "image/bmp");
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    @Autowired
+    private UploadProperties prop;
+
+//    private static final List<String> ALLOW_TYPES = Arrays.asList("image/png", "image/jpeg", "image/bmp");
 
     @Override
     public String upload(MultipartFile file) {
         try {
             // 校验文件类型
             String type = file.getContentType();
-            if (!ALLOW_TYPES.contains(type)) {
+            if (!prop.getAllowTypes().contains(type)) {
                 throw new LyException(ExceptionEnum.INVALID_FILE_TYPE);
             }
 
@@ -36,15 +50,20 @@ public class UploadServiceImpl implements UploadService {
             }
 
             // 准备目标路径
-            File dest = new File("D://heima/upload/", file.getOriginalFilename());
+//            File dest = new File("D://heima/upload/", file.getOriginalFilename());
             // 保存文件到本地
-            file.transferTo(dest);
+//            file.transferTo(dest);
+
+//            String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+            String extension = StringUtils.substringAfterLast(file.getOriginalFilename(), ".");
+            // 上传至FastDFS
+            StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
 
             // 返回路径
-            return "http://image.leyou.com/" + file.getOriginalFilename();
+            return prop.getBaseUrl() + storePath.getFullPath();
         } catch (IOException e) {
             // 上传失败
-            log.error("上传文件失败！", e);
+            log.error("[文件上传] 上传文件失败！", e);
             throw new LyException(ExceptionEnum.UPLOAD_FILE_ERROR);
         }
     }
